2c5c21dfed336d58ebada98c87deabaafaa9bbc3
Seu Nome <appmotoristapro@gmail.com>
2025-12-23 22:10:06 -0300

    Ajustando

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.motoristapro.android"
         minSdk = 26 // MediaProjection requer minSdk mais alto para funcionar bem
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
